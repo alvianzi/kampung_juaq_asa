8e3dde5ef1954a322f5763a52de591ee68bc32be
alvianzi <90459641+alvianzi@users.noreply.github.com>
2024-10-07 16:27:49 +0800

    Add files via upload

---
+++ Kampung Juaq Asa/data.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data - Kampung Juaq Asa</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/6fd4e8e126.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/data.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top p-3" id="navbar">
+        <div class="container">
+            <img class="logo me-2" src="logo/logojuaq.png" alt="" height="27px">
+            <a class="navbar-brand" href="index.php">KAMPUNG JUAQ ASA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+                    <li class="nav-item me-3">
+                        <a class="nav-link active utama" aria-current="page" href="index.php">Beranda</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link active utama" aria-current="page" href="tentang.php">Tentang</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link active utama" aria-current="page" href="data.php">Data</a>
+                    </li>
+                    <li class="nav-item me-4">
+                        <a class="nav-link active utama" aria-current="page" href="faq.php">FAQ</a>
+                    </li>
+                    <li class="nav-item" id="highlight">
+                        <a class="nav-link active rounded border border-light" aria-current="page" href="programkerja.php">Program Kampung</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#F6F6F6" fill-opacity="1"
+            d="M0,288L48,250.7C96,213,192,139,288,128C384,117,480,171,576,176C672,181,768,139,864,149.3C960,160,1056,224,1152,213.3C1248,203,1344,117,1392,74.7L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
+        </path>
+    </svg>
+    <section>
+        <div class="container">
+            <h1>DATA STATISTIK KAMPUNG JUAQ ASA</h1>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Gambar</th>
+                        <th>Judul</th>
+                        <th>Verifikator</th>
+                        <th>Tahun</th>
+                        <th>Pratinjau</th>
+                        <th>Download</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><img src="datajuaq/buku1.jpeg" alt="monografi" width="150"></td>
+                        <td>DATA MONOGRAFI JUAQ ASA</td>
+                        <td>Pemerintah Kampung Juaq Asa</td>
+                        <td>2024</td>
+                        <td><a href="datajuaq/monografi.pdf" class="view-btn" target="_blank"><i class="fas fa-eye"></i> Pratinjau</a></td>
+                        <td><a href="datajuaq/monografi.pdf" class="download-btn" download="monografi.pdf"><i class="fas fa-download"></i> Download</a></td>
+                    </tr>
+                    <tr>
+                        <td><img src="datajuaq/buku2.jpeg" alt="publikasi" width="50"></td>
+                        <td>PUBLIKASI SOSIAL EKONOMI</td>
+                        <td>Pemerintah Kampung Juaq Asa</td>
+                        <td>2024</td>
+                        <td><a href="datajuaq/publikasi.pdf" class="view-btn" target="_blank"><i class="fas fa-eye"></i> Pratinjau</a></td>
+                        <td><a href="datajuaq/publikasi.pdf" class="download-btn" download="publikasi.pdf"><i class="fas fa-download"></i> Download</a></td>
+                    </tr>
+                    <tr>
+                        <td><img src="datajuaq/kependudukan.png"  alt="kependudukan" width="50"></td>
+                        <td>DATA KEPENDUDUKAN</td>
+                        <td>Pemerintah Kampung Juaq Asa</td>
+                        <td>2024</td>
+                        <td><a href="datajuaq/kependudukan.png" class="view-btn" target="_blank"><i class="fas fa-eye"></i> Pratinjau</a></td>
+                        <td><a href="datajuaq/kependudukan.png" class="download-btn" download="kependudkan.png"><i class="fas fa-download"></i> Download</a></td>
+                    </tr>
+                    <tr>
+                        <td><img src="datajuaq/pendidikan.png"  alt="pendidikan" width="50"></td>
+                        <td>DATA PENDIDIKAN</td>
+                        <td>Pemerintah Kampung Juaq Asa</td>
+                        <td>2024</td>
+                        <td><a href="datajuaq/pendidikan.png" class="view-btn" target="_blank"><i class="fas fa-eye"></i> Pratinjau</a></td>
+                        <td><a href="datajuaq/pendidikan.png" class="download-btn" download="pendidikan.png"><i class="fas fa-download"></i> Download</a></td>
+                    </tr>
+                    <tr>
+                        <td><img src="datajuaq/kesehatan.png"  alt="kesehatan" width="50"></td>
+                        <td>DATA KESEHATAN</td>
+                        <td>Pemerintah Kampung Juaq Asa</td>
+                        <td>2024</td>
+                        <td><a href="datajuaq/kesehatan.png" class="view-btn" target="_blank"><i class="fas fa-eye"></i> Pratinjau</a></td>
+                        <td><a href="datajuaq/kesehatan.png" class="download-btn" download="kesehatan.png"><i class="fas fa-download"></i> Download</a></td>
+                    </tr>
+                    <tr>
+                        <td><img src="datajuaq/perumahan.png"  alt="perumahan" width="50"></td>
+                        <td>DATA PERUMAHAN</td>
+                        <td>Pemerintah Kampung Juaq Asa</td>
+                        <td>2024</td>
+                        <td><a href="datajuaq/perumahan.png" class="view-btn" target="_blank"><i class="fas fa-eye"></i> Pratinjau</a></td>
+                        <td><a href="datajuaq/perumahan.png" class="download-btn" download="perumahan.png"><i class="fas fa-download"></i> Download</a></td>
+                    </tr>
+                    <tr>
+                        <td><img src="datajuaq/ketenagakerjaan.png"  alt="ketenagakerjaan" width="50"></td>
+                        <td>DATA KETENAGAKERJAAN</td>
+                        <td>Pemerintah Kampung Juaq Asa</td>
+                        <td>2024</td>
+                        <td><a href="datajuaq/ketenagakerjaan.png" class="view-btn" target="_blank"><i class="fas fa-eye"></i> Pratinjau</a></td>
+                        <td><a href="datajuaq/ketenagakerjaan.png" class="download-btn" download="ketenagakerjaan.png"><i class="fas fa-download"></i> Download</a></td>
+                    </tr>
+                    <tr>
+                        <td><img src="datajuaq/aset.png"  alt="aset" width="50"></td>
+                        <td>DATA ASET</td>
+                        <td>Pemerintah Kampung Juaq Asa</td>
+                        <td>2024</td>
+                        <td><a href="datajuaq/aset.png" class="view-btn" target="_blank"><i class="fas fa-eye"></i> Pratinjau</a></td>
+                        <td><a href="datajuaq/aset.png" class="download-btn" download="aset.png"><i class="fas fa-download"></i> Download</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#F6F6F6" fill-opacity="1"
+            d="M0,288L48,250.7C96,213,192,139,288,128C384,117,480,171,576,176C672,181,768,139,864,149.3C960,160,1056,224,1152,213.3C1248,203,1344,117,1392,74.7L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
+        </path>
+    </svg>
+    <section class="mt-5">
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center mt-3">
+                        <p>© 2024 KAMPUNG JUAQ ASA. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3Z9C5W5m4gyxqNhXNn6f58HNfD1XtzpT3ME0yNgOvTMO1nItCckKfYENNIHpl5" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Kampung Juaq Asa/programkerja.php
@@ -0,0 +1,273 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Publikasi dan Indografis - Kampung Juaq Asa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/6fd4e8e126.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/programkerja.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top p-3" id="navbar">
+        <div class="container">
+            <img class="logo me-2" src="logo/logojuaq.png" alt="" height="27px">
+            <a class="navbar-brand" href="index.php">KAMPUNG JUAQ ASA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+                    <li class="nav-item me-3">
+                        <a class="nav-link active utama" aria-current="page" href="index.php">Beranda</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link active utama" aria-current="page" href="tentang.php">Tentang</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link active utama" aria-current="page" href="data.php">Data</a>
+                    </li>
+                    <li class="nav-item me-4">
+                        <a class="nav-link active utama" aria-current="page" href="faq.php">FAQ</a>
+                    </li>
+                    <li class="nav-item" id="highlight">
+                        <a class="nav-link active rounded border border-light" aria-current="page" href="programkerja.php">Program Kampung</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section class="hero_proker pt-3">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col text-center">
+                    <h4 class="lokasi">PELATIHAN PEWARNAAN DAN PACKING ANYAMAN ROTAN</h4>
+                    <p>PROGRAM KAMPUNG JUAQ ASA</p>
+                </div>
+            </div>
+            <div class="row justify-content-center text-center">
+                <div class="col-6">
+                    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="dokumentasi/rotan4.jpeg" class="d-block w-100 rounded" alt="gapura_kampung">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PROGRAM KAMPUNG JUAQ ASA</h5>
+                                    <p>PELATIHAN PEWARNAAN DAN PACKING ANYAMAN ROTAN</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/rotan7.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEWARNAAN DAN PACKING ANYAMAN ROTAN</h5>
+                                    <p>Pemaparan materi untuk pewarnaan dan packing anyaman rotan</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/rotan6.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEWARNAAN DAN PACKING ANYAMAN ROTAN</h5>
+                                    <p>Proses pewarnaan rotan</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/rotan1.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEWARNAAN DAN PACKING ANYAMAN ROTAN</h5>
+                                    <p>Proses anyaman rotan</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/rotan2.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEWARNAAN DAN PACKING ANYAMAN ROTAN</h5>
+                                    <p>Proses anyaman rotan</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/rotan5.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEWARNAAN DAN PACKING ANYAMAN ROTAN</h5>
+                                    <p>Proses anyaman rotan</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/rotan3.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEWARNAAN DAN PACKING ANYAMAN ROTAN</h5>
+                                    <p>Proses pewarnaan rotan</p>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="row mb-4">
+                        <div class="col-12">
+                            <img class="rounded" src="dokumentasi/rotan8.jpeg" alt="" width="100%" height="100%">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <img class="rounded" src="dokumentasi/rotan9.jpeg" alt="" width="100%" height="100%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col text-center">
+                    <h4 class="lokasi">PELATIHAN PEMBUATAN SAMPO UNTUK MENINGKATKAN SDM </h4>
+                    <p>PROGRAM KAMPUNG JUAQ ASA</p>
+                </div>
+            </div>
+            <div class="row justify-content-center text-center">
+                <div class="col-6">
+                    <div id="carouseltujuhbelasan" class="carousel slide" data-bs-ride="false">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                            <button type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide-to="9" aria-label="Slide 10"></button>
+                           
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="dokumentasi/sampo1.jpeg" class="d-block w-100 rounded" alt="gapura_kampung">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>Pencarian bahan untuk diolah menjadi sampo</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/sampo2.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>pemaparan materi tentang proses pembuatan sampo</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/sampo3.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>pemaparan materi tentang proses pembuatan sampo</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/sampo4.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>pemaparan materi tentang proses pembuatan sampo</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/sampo5.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>pemaparan materi tentang proses pembuatan sampo</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/sampo6.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>Praktik pembuatan sampo</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/sampo7.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>Praktik pembuatan sampo</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/sampo8.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>Praktik pembuatan sampo</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="dokumentasi/sampo9.jpeg" class="d-block w-100 rounded" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>PELATIHAN PEMBUATAN SAMPO</h5>
+                                    <p>Lomba Bulutangkis Putra</p>
+                                </div>
+                            </div>
+</div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouseltujuhbelasan" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="row mb-4 ">
+                        <div class="col-12">
+                            <img class="rounded" src="dokumentasi/sampo10.jpeg" alt="" width="100%" height="100%">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <img class="rounded" src="dokumentasi/sampo11.jpeg" alt="" width="100%" height="100%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#F6F6F6" fill-opacity="1" d="M0,288L48,250.7C96,213,192,139,288,128C384,117,480,171,576,176C672,181,768,139,864,149.3C960,160,1056,224,1152,213.3C1248,203,1344,117,1392,74.7L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+    </svg>
+
+    <section class=" mt-5">
+        <div class="footer fixed-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center mt-3">
+                        <p>Copyright © 2024 - PEMERINTAHAN KAMPUNG JUAQ ASA. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="js/FAQ.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
